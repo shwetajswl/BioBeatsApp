ce6fd4ed1a0c335ceb0ed603b6c66b4facce4546
Sweta Jaiswal <shwetajaiswal62@gmail.com>
2019-05-14 14:05:56 +0100

    Removed unwanted code

--- Testing/BioBeatsAppUITests/Movies List/MoviesListUITest.swift
+++ Testing/BioBeatsAppUITests/Movies List/MoviesListUITest.swift
@@ -11,15 +11,7 @@ import XCTest
 class MoviesListUITest: BaseUITestCase {
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        super.setUp()
     }
 
     override func tearDown() {
@@ -31,6 +23,7 @@ class MoviesListUITest: BaseUITestCase {
     func testUIElements() {
         
         launchApp()
+        
     }
 
 }
